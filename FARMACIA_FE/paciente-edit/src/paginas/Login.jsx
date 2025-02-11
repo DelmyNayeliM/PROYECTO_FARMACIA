01a99e586cbf30614e685cd14b5a1489805605f4
DelmyNayeliM <deln_mezap@unicah.edu>
2025-02-11 15:28:09 -0600

    Update Login.jsx

--- FARMACIA_FE/paciente-edit/src/paginas/Login.jsx
+++ FARMACIA_FE/paciente-edit/src/paginas/Login.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+//import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [tipo_usuario, setTipo_usuario] = useState('');
   const [nombre, setNombre] = useState('');
   const [password, setPassword] = useState('');
+  //let navigate= useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log({ tipo_usuario, nombre, password });
-  };
-
+ 
+    try {
+        const response = await axios.post("http://localhost:3003/usuarios/login", {
+            nombre: tipo_usuario,   // Revisa que este valor no sea undefined
+            contraseña: password
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error de login:', error.response ? error.response.data : error.message);
+    }
+};
   return (
     <div className="site-wrap">
       <div className="site-section"></div>
