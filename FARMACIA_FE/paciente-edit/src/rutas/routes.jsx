18fae5c9691f3f526e4225eeedfe6075a6197b3b
DelmyNayeliM <deln_mezap@unicah.edu>
2025-02-14 14:26:47 -0600

    Update routes.jsx

--- FARMACIA_FE/paciente-edit/src/rutas/routes.jsx
+++ FARMACIA_FE/paciente-edit/src/rutas/routes.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Navigate, Route, createBrowserRouter, createRoutersFromElements} from 'react-router-dom';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
-import form_medicamento from './paginas/medicamento';
+import Form_medicamento from './paginas/medicamento';
 import Formulariopaciente from './form_paciente';
 import Formulariocitas from './paginas/form_citas';
 import Acercade from '../paginas/acercade';
 import Navbar from '../plantilla/navbar';
 
 
-
 export const routers = createBrowserRouter(
-    createRoutersFromElements(
-        <Route>
-            <Navbar />
-            <Route path="/" element={<Navigate to="/form_medicamento" />} />
-            <Route path='/medicamento' element={<form_medicamento />} />
+    createRoutesFromElements(
+        <Route element={<Navbar />}>
+            <Route path="/" element={<Navigate to="/medicamento" />} />
+            <Route path='/medicamento' element={<Form_medicamento />} />
             <Route path='/Formulariopaciente' element={<Formulariopaciente />} />
             <Route path='/Formulariocitas' element={<Formulariocitas />} />
             <Route path='/Acercade' element={<Acercade />} />
         </Route>
-    ),
+    )
 );
-
