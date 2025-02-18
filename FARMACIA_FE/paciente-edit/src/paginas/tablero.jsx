b14f63ddc0117f04d8bea4a29b40963e7db687f3
DelmyNayeliM <145404286+DelmyNayeliM@users.noreply.github.com>
2025-02-18 10:04:12 -0600

    Update tablero.jsx

--- FARMACIA_FE/paciente-edit/src/paginas/tablero.jsx
+++ FARMACIA_FE/paciente-edit/src/paginas/tablero.jsx
@@ -6,46 +6,46 @@ const Tablero = () => {
       id: 12,
       date: "1 Oct, 21",
       status: "Paid",
-      customer: "Althan Travis",
+      //customer: "Althan Travis",
       purchase: "Wirecard for figma",
       revenue: "$0.99",
-      customerImg: "https://i.imgur.com/VKOeFyS.png",
+      //customerImg: "https://i.imgur.com/VKOeFyS.png",
     },
     {
       id: 14,
       date: "12 Oct, 21",
       status: "Failed",
-      customer: "Tomo arvis",
+      //customer: "Tomo arvis",
       purchase: "Altroz furry",
       revenue: "$0.19",
-      customerImg: "https://i.imgur.com/nmnmfGv.png",
+      //customerImg: "https://i.imgur.com/nmnmfGv.png",
     },
     {
       id: 17,
       date: "1 Nov, 21",
       status: "Paid",
-      customer: "Althan Travis",
+      //customer: "Althan Travis",
       purchase: "Apple Macbook air",
       revenue: "$1.99",
-      customerImg: "https://i.imgur.com/VKOeFyS.png",
+      //customerImg: "https://i.imgur.com/VKOeFyS.png",
     },
     {
       id: 90,
       date: "19 Oct, 21",
       status: "Paid",
-      customer: "Travis head",
+      //customer: "Travis head",
       purchase: "Apple Macbook Pro",
       revenue: "$9.99",
-      customerImg: "https://i.imgur.com/VKOeFyS.png",
+      //customerImg: "https://i.imgur.com/VKOeFyS.png",
     },
     {
       id: 12,
       date: "1 Oct, 21",
       status: "Paid",
-      customer: "Althan Travis",
+      //customer: "Althan Travis",
       purchase: "Wirecard for figma",
       revenue: "$0.99",
-      customerImg: "https://i.imgur.com/nmnmfGv.png",
+      //customerImg: "https://i.imgur.com/nmnmfGv.png",
     }
   ];
 
@@ -56,11 +56,10 @@ const Tablero = () => {
           <span className="position-absolute search">
             <i className="fa fa-search"></i>
           </span>
-          <input className="form-control w-100" placeholder="Search by order#, name..." />
+          <input className="form-control w-100" placeholder="Buscar por nombre" />
         </div>
 
         <div className="px-2">
-          <span>Filters <i className="fa fa-angle-down"></i></span>
           <i className="fa fa-ellipsis-h ms-3"></i>
         </div>
       </div>
@@ -73,12 +72,11 @@ const Tablero = () => {
                 <input className="form-check-input" type="checkbox" />
               </th>
               <th scope="col" width="5%">#</th>
-              <th scope="col" width="20%">Date</th>
-              <th scope="col" width="10%">Status</th>
-              <th scope="col" width="20%">Customer</th>
-              <th scope="col" width="20%">Purchased</th>
+              <th scope="col" width="10%">Fecha</th>
+              <th scope="col" width="10%">Estado</th>
+              <th scope="col" width="20%">Medicamento</th>
               <th scope="col" className="text-end" width="20%">
-                <span>Revenue</span>
+                <span>Precio</span>
               </th>
             </tr>
           </thead>
@@ -94,10 +92,6 @@ const Tablero = () => {
                 <td>
                   <i className={`fa fa-${item.status === 'Paid' ? 'check-circle-o green' : 'dot-circle-o text-danger'}`}></i>
                   <span className="ms-1">{item.status}</span>
-                </td>
-                <td>
-                  <img src={item.customerImg} width="25" alt="Customer" />
-                  {item.customer}
                 </td>
                 <td>{item.purchase}</td>
                 <td className="text-end">
