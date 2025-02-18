209145bd540bd4b03b2402c2af07c4af2cd85ef8
DelmyNayeliM <deln_mezap@unicah.edu>
2025-02-18 08:22:02 -0600

    Update rutas_login.js

--- FARMACIA_BE/src/rutas/rutas_login.js
+++ FARMACIA_BE/src/rutas/rutas_login.js
@@ -1,1 +1,8 @@
+const { Router } = require('express');
+const Cont_log = require('../controladores/controlador_login');
+const { contextsKey } = require('express-validator/lib/base');
+const rutas=Router();
 
+rutas.post('/login', Cont_log.login);
+
+module.exports=rutas;
