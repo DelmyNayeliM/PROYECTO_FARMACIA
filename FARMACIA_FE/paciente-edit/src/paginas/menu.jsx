84e64647def9001bf61589570da4df5380d5fac5
DelmyNayeliM <145404286+DelmyNayeliM@users.noreply.github.com>
2025-02-18 13:52:17 -0600

    Update menu.jsx

--- FARMACIA_FE/paciente-edit/src/paginas/menu.jsx
+++ FARMACIA_FE/paciente-edit/src/paginas/menu.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Inicio = () => {
+  const menuItems = [
+    { title: 'Citas', path: '/formulariocitas' },
+    { title: 'Pacientes', path: '/formulariopaciente' },
+    { title: 'Inventario', path: '/tablero' }
+  ];
+
   return (
     <main className="site-wrapper">
       <div className="pt-table desktop-768">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-offset-2">
+              <div className="page-title home text-center"></div>
 
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-offset-2 ">
-                <div className="page-title home text-center">
-                </div>
-
-                <div className="hexagon-menu clear">
-                  {['citas', 'PACIENTES', 'inventario',].map((title, index) => (
-                    <div className="hexagon-item" key={index}>
-                      <div className="hex-item">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                      </div>
-                      <div className="hex-item">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                      </div>
-                      <a className="hex-content">
-                        <span className="hex-content-inner">
-                          <span className="title">{title}</span>
-                        </span>
-                      </a>
+              <div className="hexagon-menu clear">
+                {menuItems.map((item, index) => (
+                  <div className="hexagon-item" key={index}>
+                    <div className="hex-item">
+                      <div></div>
+                      <div></div>
+                      <div></div>
                     </div>
-                  ))}
-                </div>
+                    <div className="hex-item">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                    <Link to={item.path} className="hex-content">
+                      <span className="hex-content-inner text-black">
+                        <span className="title">{item.title}</span>
+                      </span>
+                    </Link>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
+        </div>
       </div>
     </main>
   );
