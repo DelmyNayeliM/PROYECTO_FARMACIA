277b09eace295323e98337cd6615829aa5f30a41
DelmyNayeliM <145404286+DelmyNayeliM@users.noreply.github.com>
2025-02-18 13:52:12 -0600

    Update index.js

--- FARMACIA_FE/paciente-edit/src/index.js
+++ FARMACIA_FE/paciente-edit/src/index.js
@@ -11,10 +11,9 @@ import './css/owl.carousel.min.css';
 import './css/owl.theme.default.min.css';
 import './css/menu.css';
 
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Form_medicamento from './paginas/medicamento';
-//import FormularioRegistro from './paginas/form_registro';
 import Formulariopaciente from './paginas/form_paciente';
 import Formulariocitas from './paginas/form_citas';
 import Acercade from './paginas/acercade';
@@ -24,23 +23,23 @@ import Formulario from './paginas/imprimir';
 import Inicio from './paginas/menu';
 import Tablero from './paginas/tablero';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>  
       <div>
-      <Inicio />
-        <Login />
         <Navbar />
-          <Formulariopaciente />
-        <Navbar />
-          <Formulariocitas />
-          <Tablero />
-        <Acercade />
-        <Formulario />
-        <Form_medicamento />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/medicamento" element={<Form_medicamento />} />
+          <Route path="/formulariopaciente" element={<Formulariopaciente />} />
+          <Route path="/formulariocitas" element={<Formulariocitas />} />
+          <Route path="/acercade" element={<Acercade />} />
+          <Route path="/tablero" element={<Tablero />} />
+          <Route path="/formulario" element={<Formulario />} />
+        </Routes>
       </div>
     </BrowserRouter>
   </React.StrictMode>
