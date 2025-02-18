f4c932b8a0bc0b39cc2352f4bb1c4f198e04a86d
DelmyNayeliM <145404286+DelmyNayeliM@users.noreply.github.com>
2025-02-18 13:52:22 -0600

    Update routes.jsx

--- FARMACIA_FE/paciente-edit/src/rutas/routes.jsx
+++ FARMACIA_FE/paciente-edit/src/rutas/routes.jsx
@@ -9,18 +9,16 @@ import Navbar from '../plantilla/navbar';
 import Inicio from '../paginas/menu';
 import Tablero from '../paginas/tablero';
 
-
 export const routers = createBrowserRouter(
     createRoutesFromElements(
-        <Route element={<Navbar />}>
-            <Route element={<Inicio />}>
-            <Route path="/" element={<Navigate to="/medicamento" />} />
-            <Route path='/medicamento' element={<Form_medicamento />} />
-            <Route path='/Formulariopaciente' element={<Formulariopaciente />} />
-            <Route path='/Formulariocitas' element={<Formulariocitas />} />
-            <Route path='/Acercade' element={<Acercade />} />
-            <Route path='/Tablero' element={<Tablero />} />
-        </Route>
-        </Route>
+    <Route element={<Navbar />}>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<Navigate to="/medicamento" />} /> 
+        <Route path="/medicamento" element={<Form_medicamento />} />
+        <Route path="/Formulariopaciente" element={<Formulariopaciente />} />
+        <Route path="/Formulariocitas" element={<Formulariocitas />} />
+        <Route path="/Acercade" element={<Acercade />} />
+        <Route path="/Tablero" element={<Tablero />} />
+    </Route>
     )
 );
